af3bee5df92dd9a33594693b6070837e11da4ee1
Rohan Maharjan <rohan.maharjan@harness.io>
2024-09-18 20:39:22 +0545

    google ste ownership verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,9 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
 
-import styles from './index.module.css';
-import { useEffect } from 'react';
-import { useLocation } from '@docusaurus/router';
 import BrowserOnly from '@docusaurus/BrowserOnly';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -40,27 +12,24 @@ export default function Home(): JSX.Element {
     <BrowserOnly fallback={<div>Loading...</div>}>
       {() => {
         const rootUrl = window.location.href.split('/').slice(0, 3).join('/');
-        console.log(rootUrl);
 
-        useEffect(() => {
-          async function callAPI() {
-            const res = await fetch(rootUrl + '/api/coveo_proxy');
-
-            const data = await res.json();
-            // console.log(data);
-          }
-          callAPI();
-        }, []);
         return (
-          <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
-          >
-            <HomepageHeader />
-            <main>
-              <HomepageFeatures />
-            </main>
-          </Layout>
+          <>
+            <Head>
+              <meta
+                name="google-site-verification"
+                content="dHh5c5B9VBRFZTjNEITlh3SS4o5qjHtdMCIVbqzCknc"
+              />
+            </Head>
+            <Layout
+              title={`Hello from ${siteConfig.title}`}
+              description="Description will go into a meta tag in <head />"
+            >
+              <main>
+                <HomepageFeatures />
+              </main>
+            </Layout>
+          </>
         );
       }}
     </BrowserOnly>
