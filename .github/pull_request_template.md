655b61fcce048abc27b21e4dbb89976d46524840
Rohan Maharjan <rohan.maharjan@harness.io>
2025-05-31 09:40:36 +0545

    Initial commit (history reset)

---
+++ .github/pull_request_template.md
@@ -0,0 +1,18 @@
+Thanks for contributing to the Harness Developer Hub! Our code owners will review your submission.
+
+## Pre-flight checklist
+
+- [ ] I have read the [Contributing Guidelines on pull requests].
+
+## Description
+
+* Please describe your changes: 
+* Jira/GitHub Issue numbers (if any): 
+* Preview links/images (Internal contributors only): 
+
+## PR lifecycle
+
+We aim to merge PRs within one week or less, but delays happen sometimes.
+
+If your PR is open longer than two weeks without any human activity, please tag a [code owner](https://github.com/harness/developer-hub/blob/main/.github/CODEOWNERS) in a comment.
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Layout from '@theme/Layout';
+
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import Head from '@docusaurus/Head';
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        const rootUrl = window.location.href.split('/').slice(0, 3).join('/');
+
+
+        
+        return (
+          <>
+            <Layout
+              title={`Hello from ${siteConfig.title}`}
+              description="Description will go into a meta tag in <head />"
+            >
+              <Head>
+                <meta
+                  name="google-site-verification"
+                  content="dHh5c5B9VBRFZTjNEITlh3SS4o5qjHtdMCIVbqzCknc"
+                />
+              </Head>
+              <main>
+                <HomepageFeatures />
+              </main>
+            </Layout>
+          </>
+        );
+      }}
+    </BrowserOnly>
+  );
+}
